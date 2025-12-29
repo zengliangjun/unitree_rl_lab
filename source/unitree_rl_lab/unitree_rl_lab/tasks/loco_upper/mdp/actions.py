98ada439de10882e912eaa7af9ecb7f42542c6b9
liangjunzeng <liangjunzeng@163.com>
2025-12-29 08:32:09 +0800

    #Fix actions error

--- source/unitree_rl_lab/unitree_rl_lab/tasks/loco_upper/mdp/actions.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/loco_upper/mdp/actions.py
@@ -41,8 +41,17 @@ class MotionActions(ActionTerm):
         else:
             raise ValueError(f"Unsupported scale type: {type(cfg.scale)}. Supported types are float and dict.")
 
-        ##
-        self.motions_max_idx = []
+        ## sdk index
+        sdk_names = []
+        for name in  env.cfg.scene.robot.joint_sdk_names:
+            if 0 == len(name):
+                continue
+            sdk_names.append(name)
+
+        sdk_indexes = []
+        for name in cfg.joint_names:
+            sdk_indexes.append(sdk_names.index(name))
+
         self.motions_dof_pos = []
 
         episode_length = int(env.cfg.episode_length_s * (1 / env.step_dt)) + 10
@@ -55,30 +64,13 @@ class MotionActions(ActionTerm):
                 full_name = os.path.join(dirpath, filename)
                 with open(full_name, "rb") as fd:
                     motions = pickle.load(fd)
-                    motions = motions["dof_pos"]
-                    #if motions.shape[0] < episode_length:
-                    #    continue
-
+                    motions = motions["dof_pos"][:, sdk_indexes]
+                    motions = torch.tensor(motions, dtype=torch.float32, device=env.device)
                     self.motions_dof_pos.append(motions)
-                    self.motions_max_idx.append(motions.shape[0])
 
-        self.motions_max_idx = torch.tensor(self.motions_max_idx, dtype = torch.float32)
-        self.motions_probs = self.motions_max_idx.float() / torch.sum(self.motions_max_idx)
-
-        sdk_names = []
-        for name in  env.cfg.scene.robot.joint_sdk_names:
-            if 0 == len(name):
-                continue
-            sdk_names.append(name)
-
-        self.sdk_indexes = []
-        for name in cfg.joint_names:
-            self.sdk_indexes.append(sdk_names.index(name))
-
-        self.current_idx = torch.zeros((self.num_envs), dtype=torch.long)
-        self.action_motion_idx = torch.zeros((self.num_envs), dtype=torch.long)
-        self.motion_dir = torch.zeros((self.num_envs), dtype=torch.int)
-
+        self.motions_dof_pos = torch.cat(self.motions_dof_pos, dim=0)
+        self.max_idx = self.motions_dof_pos.shape[0] - episode_length
+        self.current_idx = torch.zeros((self.num_envs), dtype=torch.long, device=env.device)
 
     """
     Properties.
@@ -105,36 +97,19 @@ class MotionActions(ActionTerm):
     def reset(self, env_ids: Sequence[int] | None = None) -> None:
         if env_ids is None:
             env_ids = slice(None)
-
             count = self.num_envs
         else:
             count = len(env_ids)
 
-        selected_idxs = torch.multinomial(self.motions_probs, num_samples=count, replacement=True)
-        self.action_motion_idx[env_ids] = selected_idxs
-        max_idx = self.motions_max_idx[selected_idxs]
-        self.current_idx[env_ids] = torch.tensor(torch.rand_like(selected_idxs, dtype = torch.float32) * max_idx, dtype= torch.long)
-        self.motion_dir[env_ids] = 1
+        selected_idxs = torch.randint(high=self.max_idx, size=(count,), dtype=torch.long, device=self._env.device)
+        self.current_idx[env_ids] = selected_idxs
 
     def apply_actions(self):
-        poses = []
-        for mid, indx in zip(self.action_motion_idx, self.current_idx):
-            dof_pos = self.motions_dof_pos[mid]
-            mid, indx = mid.item(), indx.item()
-
-            dof_pos = torch.tensor(dof_pos[indx: indx + 1][:, self.sdk_indexes], dtype=torch.float32)
-            poses.append(dof_pos)
-
-        self.current_idx += self.motion_dir[:]
-        over_min = self.current_idx < 0
-        self.current_idx[over_min] = 1
-        self.motion_dir[over_min] = 1
-
-        max_idx = self.motions_max_idx[self.action_motion_idx]
-        over_max = (self.current_idx - max_idx) >= 0
-        self.current_idx[over_max] = self.current_idx[over_max] - 1
-        self.motion_dir[over_min] = - 1
-
-        pos = torch.cat(poses, dim=0)
-        self._raw_actions[ :] = pos.to(self._env.device)
+        pos = self.motions_dof_pos[self.current_idx]
+        self._raw_actions[:] = pos
         self._asset.set_joint_position_target(self.processed_actions, joint_ids=self._joint_ids)
+        self.current_idx += 1
+
+        reset_flags = self.motions_dof_pos >= (self.motions_dof_pos.shape[0] -1)
+        if torch.sum(reset_flags) > 0:
+            self.current_idx[reset_flags] = 0
