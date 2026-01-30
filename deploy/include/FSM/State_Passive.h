5f3e4c3f48efa52c2fe61b68bea0eb51e74d586f
liangjun <liangjunzeng@163.com>
2026-01-30 08:50:15 +0800

    #Fix test code

--- deploy/include/FSM/State_Passive.h
+++ deploy/include/FSM/State_Passive.h
@@ -54,28 +54,56 @@ public:
         FSMState::pre_run();
         robot_->update();
         if (joystick_checks_()) {
+            update_q = true;
             dump_data_type ++;
-            dump_data_type = dump_data_type % 3;
+            dump_data_type = dump_data_type % 39;
             std::cout << "dump_data_type: " << dump_data_type << std::endl;
         }
-        if (dump_data_type == 1) {
-            std::cout << " ang_vel: " << robot_->data.root_ang_vel_b.transpose() <<
-                         " gravity_b: " << robot_->data.projected_gravity_b.transpose() <<
-                         " quat_w: " << robot_->data.root_quat_w << std::endl;
-        }
+
+        // if (dump_data_type == 1) {
+        //     std::cout << "R ang_vel: " << robot_->data.root_ang_vel_b.transpose() <<
+        //                  "R gravity_b: " << robot_->data.projected_gravity_b.transpose() <<
+        //                  "R quat_w: " << robot_->data.root_quat_w << std::endl;
+        //     std::cout << "R q0: " << lowstate->msg_.motor_state()[0].q() << std::endl;
+        // }
     }
 
     void run()
     {
         for(int i(0); i < lowcmd->msg_.motor_cmd().size(); ++i)
         {
-            lowcmd->msg_.motor_cmd()[i].q() = 12; // lowstate->msg_.motor_state()[i].q();
-            if (i == 0 && dump_data_type == 2) {
-                std::cout << " motor_cmd[0].q(): " << lowcmd->msg_.motor_cmd()[i].q() << std::endl;
+            int s = dump_data_type % 13;
+            if (update_q && s == i) {
+
+                int i_mod = dump_data_type / 13;
+
+                if (i_mod == 0) {
+                    lowcmd->msg_.motor_cmd()[i].q() = 0.1;
+                } else if (i_mod == 1) {
+                    lowcmd->msg_.motor_cmd()[i].q() = 0.1;
+                } else if (i_mod == 2) {
+                    lowcmd->msg_.motor_cmd()[i].q() = 0.1;
+                }
+                std::cout << "Set motor: " << i << " to q: " << lowcmd->msg_.motor_cmd()[i].q() << std::endl;
+            }
+            // if (update_q && 9 == i) {
+            //     if (dump_data_type % 2 == 0) {
+            //         lowcmd->msg_.motor_cmd()[i].q() = 0.2;
+            //     } else {
+            //         lowcmd->msg_.motor_cmd()[i].q() = 0;
+            //     }
+            //     std::cout << "Set motor: " << i << " to q: " << lowcmd->msg_.motor_cmd()[i].q() << std::endl;
+            // }
+            else {
+                lowcmd->msg_.motor_cmd()[i].q() = 0; // lowstate->msg_.motor_state()[i].q();
             }
         }
+        // if (update_q) {
+        //     update_q = false;
+        // }
     }
 
+    bool update_q = false;
     int dump_data_type = -1;
     std::function<bool()> joystick_checks_;
     std::shared_ptr<isaaclab::Articulation> robot_;
